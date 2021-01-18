400dafcb0366e859b4147937b1762029271a7f16
intanibels <intan_putri_29rpl@student.smktelkom-mlg.sch.id>
2021-01-18 14:15:25 +0700

    LATIHAN 1 XR2/11

---
+++ src/latihan/LATIHAN.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan;
+/**
+ *
+ * @author Intan
+ */
+public class LATIHAN {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int bil=1; //deklarasi variabel
+        do{ //melakukan perulangan
+            //statement
+            System.out.print(bil+" "); //menampilkan hasil output bilangan
+            bil++; //bilangan akan terus bertambah
+        }while(bil<=10); //disaat bilangan sudah mencapai 10, perulangan berhenti
+    }    
+}
